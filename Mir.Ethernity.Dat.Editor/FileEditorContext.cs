2aab78ec3c207015a65795b188aa2f86a9db1f01
Bernat Vadell <bvadell@feimsoft.com>
2022-07-18 14:13:12 +0200

    create simple ui to edit dat files

---
+++ Mir.Ethernity.Dat.Editor/FileEditorContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Mir.Ethernity.Dat.Editor
+{
+    public class FileEditorContext
+    {
+        public string? FilePath { get; set; } = null;
+        public string? OriginalContent { get; set; } = null;
+        public string Content { get; set; } = string.Empty;
+        public string FileName => FilePath != null ? System.IO.Path.GetFileName(FilePath) : "New file.dat";
+        public bool IsNew => FilePath == null;
+        public bool IsModified => IsNew || OriginalContent == null || Content != OriginalContent;
+    }
+}
---
+++ Mir.Ethernity.Dat.Editor/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+
+namespace Mir.Ethernity.Dat.Editor
+{
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public FileEditorContext? FileContext { get; set; } = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtContent.IsReadOnly = true;
+            UpdateViewState();
+        }
+
+        private void File_New_Click(object sender, RoutedEventArgs e)
+        {
+            if (FileContext?.IsModified ?? false)
+            {
+                var boxResult = MessageBox.Show("You have pending changes, are you sure you want to discard them and create a new file?", "New file confirmation", MessageBoxButton.OKCancel);
+                if (boxResult != MessageBoxResult.OK) return;
+            }
+
+            FileContext = new FileEditorContext();
+            UpdateViewState();
+        }
+
+        private void File_Open_Click(object sender, RoutedEventArgs e)
+        {
+            if (FileContext?.IsModified ?? false)
+            {
+                var boxResult = MessageBox.Show("You have pending changes, are you sure you want to discard them and open a file?", "Open file confirmation", MessageBoxButton.OKCancel);
+                if (boxResult != MessageBoxResult.OK) return;
+            }
+
+            var openFileDialog = new OpenFileDialog() { Filter = "Mir DAT File|*.dat" };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                if (!File.Exists(openFileDialog.FileName))
+                {
+                    MessageBox.Show($"File not found: {openFileDialog.FileName}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                var buffer = File.ReadAllBytes(openFileDialog.FileName);
+                var output = DatEncoder.Decode(buffer);
+
+                FileContext = new FileEditorContext();
+                FileContext.FilePath = openFileDialog.FileName;
+                FileContext.OriginalContent = System.Text.Encoding.UTF8.GetString(output);
+                FileContext.Content = FileContext.OriginalContent;
+
+                UpdateViewState();
+            }
+
+        }
+
+        private void File_Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (FileContext == null || !FileContext.IsModified) return;
+
+            if (FileContext.IsNew || string.IsNullOrEmpty(FileContext.FilePath))
+            {
+                var saveFileDialog = new SaveFileDialog() { Filter = "Mir DAT File|*.dat" };
+                if (saveFileDialog.ShowDialog() == false) return;
+                FileContext.FilePath = saveFileDialog.FileName;
+            }
+
+            var buffer = System.Text.Encoding.UTF8.GetBytes(FileContext.Content);
+            var output = DatEncoder.Encode(buffer);
+
+            File.WriteAllBytes(FileContext.FilePath, output);
+
+            FileContext.OriginalContent = FileContext.Content;
+
+            UpdateViewState();
+        }
+
+        private void File_SaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            if (FileContext == null) return;
+
+            var saveFileDialog = new SaveFileDialog() { Filter = "Mir DAT File|*.dat" };
+            if (saveFileDialog.ShowDialog() == false) return;
+            FileContext.FilePath = saveFileDialog.FileName;
+
+            var buffer = System.Text.Encoding.UTF8.GetBytes(FileContext.Content);
+            var output = DatEncoder.Encode(buffer);
+
+            File.WriteAllBytes(FileContext.FilePath, output);
+
+            FileContext.OriginalContent = FileContext.Content;
+
+            UpdateViewState();
+        }
+
+
+        private void TxtContent_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (FileContext == null) return;
+            FileContext.Content = txtContent.Text;
+            UpdateViewState();
+        }
+
+        private void UpdateViewState()
+        {
+            txtContent.IsReadOnly = FileContext == null;
+            txtContent.Text = FileContext?.Content ?? "";
+            UpdateLabelStatus();
+        }
+
+        private void UpdateLabelStatus()
+        {
+            if (FileContext == null)
+            {
+                lblStatus.Content = "No file opened or created";
+                return;
+            }
+
+            lblStatus.Content = $"File name: {FileContext.FileName}, Length: {FileContext.Content.Length}, Is modified: {(FileContext.IsModified ? "Yes" : "No")}";
+        }
+    }
+}
